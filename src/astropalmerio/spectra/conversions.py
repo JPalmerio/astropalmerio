d0df263a84ff7b648a249e2068f7fbf3b1191833
Jesse Palmerio <jesse.palmerio@obspm.fr>
2023-03-08 15:37:04 +0100

    Sky emission and transmission
    (Used for zHunter)

--- src/astropalmerio/spectra/conversions.py
+++ src/astropalmerio/spectra/conversions.py
@@ -5,7 +5,11 @@ from astropy.units.quantity import Quantity
 
 log = logging.getLogger(__name__)
 
-ergscm2AA = u.Unit("erg s^-1 cm^-2 AA^-1")
+ergscm2AA = u.def_unit(
+    s="ergscm2AA",
+    represents=u.Unit("erg s^-1 cm^-2 AA^-1"),
+    format={"latex": r"\mathrm{erg\,s^{-1}\,cm^{-2}\,\mathring{A}^{-1}}"},
+)
 
 
 def _quantity_to_array(x):
@@ -51,42 +55,103 @@ def vac_to_air(wave):
 
 
 def vel_to_wave(vel, w0):
-    try:
+    """Summary
+
+    Parameters
+    ----------
+    vel : TYPE
+        Description
+    w0 : TYPE
+        Description
+
+    Returns
+    -------
+    TYPE
+        Description
+    """
+    if isinstance(vel, Quantity):
         vel = vel.to(u.km / u.s)
-    except AttributeError:
+    else:
         vel = vel * u.km / u.s
-    try:
+        log.info(
+            "Input velocity is not a quantity so it has no units." " Assuming km/s."
+        )
+
+    if isinstance(w0, Quantity):
         w0 = w0.to(u.AA)
-    except AttributeError:
+    else:
+        log.info(
+            "Input center wavelength is not a quantity so it has no units."
+            " Assuming Angstrom."
+        )
         w0 = w0 * u.AA
 
-    w = vel.to(u.AA, equivalencies=u.doppler_optical(w0))
-    return w
+    wave = vel.to(u.AA, equivalencies=u.doppler_optical(w0))
+
+    return wave if isinstance(vel, Quantity) else wave.value
 
 
-def wave_to_vel(w, w0):
-    w = w * u.AA
-    w0 = w0 * u.AA
-    v = w.to(u.km / u.s, equivalencies=u.doppler_optical(w0))
-    return v
+def wave_to_vel(wave, w0):
+    """Summary
+
+    Parameters
+    ----------
+    wave : TYPE
+        Description
+    w0 : TYPE
+        Description
+
+    Returns
+    -------
+    TYPE
+        Description
+
+    """
+    if isinstance(wave, Quantity):
+        wave = wave.to(u.AA)
+    else:
+        wave = wave * u.AA
+        log.info(
+            "Input wavelength is not a quantity so it has no units."
+            " Assuming Angstrom."
+        )
+    if isinstance(w0, Quantity):
+        w0 = w0.to(u.AA)
+    else:
+        log.info(
+            "Input center wavelength is not a quantity so it has no units."
+            " Assuming Angstrom."
+        )
+        w0 = w0 * u.AA
+
+    vel = wave.to(u.km / u.s, equivalencies=u.doppler_optical(w0))
+
+    return vel if isinstance(wave, Quantity) else vel.value
 
 
-def FWHM_v2w(vel, w0):
-    wvlg = vel.to(w0.unit, equivalencies=u.doppler_optical(w0)) - w0
-    return wvlg
+def fwhm_v2w(fwhm_v, w0):
+    # Calculate Half Width at Half Max in velocity space
+    # This is necessary because velocity to wavelength is symmetric but
+    # not linear
+    hwhm_v = fwhm_v / 2.0
+    fwhm_w = 2.0 * hwhm_v.to(w0.unit, equivalencies=u.doppler_optical(w0)) - w0
+    return fwhm_w
 
 
-def FWHM_w2v(wvlg, w0):
-    wvlg = wvlg + w0
-    vel = wvlg.to(u.km / u.s, equivalencies=u.doppler_optical(w0))
-    return vel
+def fwhm_w2v(fwhm_w, w0):
+    # Calculate Half Width at Half Max in wavelength space
+    # This is necessary because wavelength to velocity is symmetric but
+    # not linear
+    hwhm_w = w0 + fwhm_w / 2.0
+    fwhm_v = 2.0 * hwhm_w.to(u.Unit("km/s"), equivalencies=u.doppler_optical(w0))
+    return fwhm_v
 
 
-def FWHM2sigma(FWHM):
-    sigma = FWHM / (2.0 * np.sqrt(2.0 * np.log(2.0)))
+def fwhm_to_sigma(fwhm):
+    sigma = fwhm / (2.0 * np.sqrt(2.0 * np.log(2.0)))
     return sigma
 
 
-def sigma2FWHM(sigma):
-    FWHM = sigma * (2.0 * np.sqrt(2.0 * np.log(2.0)))
-    return FWHM
+def sigma_to_fwhm(sigma):
+    fwhm = sigma * (2.0 * np.sqrt(2.0 * np.log(2.0)))
+    return fwhm
